3aa597a61482c932409616d5d6e90b9fb6c19c7a
harisfaris17 <harisfaris17@gmail.com>
2023-01-23 17:03:32 +0700

    Succeed to add functionallity to advertising

---
+++ comp.h
@@ -0,0 +1,31 @@
+#ifndef COMP_H__
+#define COMP_H__
+
+#include "radio_config.h"
+
+#define LSB_8_BIT_MASK                          0xFF
+#define UPPER_8_BIT_LSB_MASK                    0xFF00
+#define LOWER_8_BIT_MSB_MASK                    0xFF0000
+#define MSB_8_BIT_MASK                          0xFF000000
+
+//#define SWAP_32_BITS(data_32bit)                   (((swap_bits(data_32bit >> 24)) << 0)    | \
+//                                                    ((swap_bits(data_32bit >> 16)) << 8)    | \
+//                                                    ((swap_bits(data_32bit >> 8)) << 16)    | \
+//                                                    ((swap_bits(data_32bit >> 0)) << 24))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ pca10100/blank/ses/Output/Release/Obj/transmitter_pca10100/thumb_crt0.d
@@ -0,0 +1,2 @@
+Output/Release/Obj/transmitter_pca10100/thumb_crt0.o: \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.10/source/thumb_crt0.s
